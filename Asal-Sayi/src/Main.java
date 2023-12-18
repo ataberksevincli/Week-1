d7c5e2db645e6e3f99bdb63688dbb3ff5d4e1eef
Ataberk Sevinçli <sevincli.ataberk@hotmail.com>
2023-12-18 17:21:16 +0300

    asal sayılar bulundu 1-100 arası

---
+++ Asal-Sayi/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+
+        for (int i = 2; i <= 100; i++) {
+            boolean asal = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    asal = false;
+                    break;
+                }
+                }if (asal){
+                System.out.println(i + " sayısı bir asal sayıdır.");
+
+            }
+        }
+    }
+}
+
